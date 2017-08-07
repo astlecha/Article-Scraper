16c9499395f567f87fce8f4ca632f17256418e56
Charlotte Astle <charlotteastle@Charlottes-MBP.local.tld>
2017-08-07 16:48:29 -0700

    sets up express app and mongo database

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-// Dependencies
+// Requires dependencies
 var express = require("express");
 var mongojs = require("mongojs");
-// Require request and cheerio. This makes the scraping possible
+// Require request and cheerio for scraping
 var request = require("request");
 var cheerio = require("cheerio");
+
+// Initializes express
+var app = express();
+
+// Configures database
+var databaseUrl = "scraped";
+var collections = ["scrapedData"];
+
+// Hooks mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error){
+	console.log("Database Error:", error);
+});
+
+app.get("/", function(req, res) {
+	console.log(res)
+})
