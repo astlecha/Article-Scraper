b16b7fa81e727da413c8c0ba7cef5bc13c199c81
Charlotte Astle <charlotteastle@Charlottes-MBP.local.tld>
2017-08-07 16:43:35 -0700

    node package installments and dependencies

---
+++ server.js
@@ -0,0 +1,6 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
