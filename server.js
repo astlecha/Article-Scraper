fdcba86200128498308ec81d546e46b7f1fe0329
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-08-07 19:52:07 -0700

    routes for scraping

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requires dependencies
 var express = require("express");
 var mongojs = require("mongojs");
+
 // Require request and cheerio for scraping
 var request = require("request");
 var cheerio = require("cheerio");
@@ -19,5 +20,42 @@ db.on("error", function(error){
 });
 
 app.get("/", function(req, res) {
-	console.log(res)
+	console.log(res);
 })
+
+app.get("/all", function(req, res) {
+  // Find all in the scrapedData collection and send "found" result to the browser
+  db.scrapedData.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Scrape data from the New York Times
+app.get("/scrape", function(req, res){
+	request("http://nytimes.com/", function(error, response, html) {
+
+	  var $ = cheerio.load(html);
+
+	  $("h2").each(function(i, element) {
+	    var title = $(element).text();
+	    var link = $(element).children().attr("href");
+	    var obj = {
+	    	title: title, 
+	    	link: link
+	    }
+	    db.scrapedData.insert(obj);
+	    console.log('object', obj);
+	  });
+
+	  res.send('Scrape Complete!');
+	});
+});
+
+app.listen(8080, function() {
+  console.log("App running on port 8080!");
+});
