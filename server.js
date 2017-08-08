85c60c567870d5e44afde361bce50e24b39165d0
Charlotte Astle <charlotteastle@calvisitor-10-105-194-23.calvisitor.1918.berkeley.edu>
2017-08-08 09:41:03 -0700

    adds mongojs npm package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requires dependencies
 var express = require("express");
 var mongojs = require("mongojs");
+var mongoose = require("mongoose");
 
 // Require request and cheerio for scraping
 var request = require("request");
@@ -16,24 +17,12 @@ var collections = ["scrapedData"];
 // Hooks mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
 db.on("error", function(error){
-	console.log("Database Error:", error);
+	console.log("Database Error: ", error);
 });
 
 app.get("/", function(req, res) {
 	console.log(res);
 })
-
-app.get("/all", function(req, res) {
-  // Find all in the scrapedData collection and send "found" result to the browser
-  db.scrapedData.find({}, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
-  });
-});
 
 // Scrape data from the New York Times
 app.get("/scrape", function(req, res){
@@ -49,11 +38,44 @@ app.get("/scrape", function(req, res){
 	    	link: link
 	    }
 	    db.scrapedData.insert(obj);
-	    console.log('object', obj);
+	    console.log("object", obj);
 	  });
 
-	  res.send('Scrape Complete!');
+	  res.send("/");
 	});
+});
+
+$("#scrape-button").on("click", function() {
+	app.get("/all", function(req, res) {
+	  // Find all in the scrapedData collection and send "found" result to the browser
+	  db.scrapedData.find({}, function(error, found) {
+	    if (error) {
+	      console.log(error);
+	    }
+	    else {
+	      alert("Found "+found.length+" new articles.");
+	      res.json(found);
+	      //Here we will append a new div for each article heading with article notes & delete buttons
+	    }
+	  });
+	});
+});
+
+$(".deleteButton").on("click", function() {
+	app.get("/", function(req, res) {
+		//Delete this article from the database
+		db.scrapedData.remove({id: this.id})
+	})
+});
+
+$(".noteButton").on("click", function() {
+	app.get("/", function(req, res) {
+		//Send a popup where user types a note
+		//Append the note into the popup (make sure they can add multiple notes)
+
+		//Update this article in the database with your new note
+		// db.scrapedData.update({id: this.id}, {$set {"note": $("#added-note")}})
+	})
 });
 
 app.listen(8080, function() {
