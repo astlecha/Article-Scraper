93c02ac915fbabaa8bedb826ac064a0f412c6138
Charlotte Astle <charlotteastle@Charlottes-MBP.local.tld>
2017-08-09 09:02:47 -0700

    appends new div with buttons for each article

--- public/app.js
+++ public/app.js
@@ -1,16 +1,19 @@
 // Gets json-version of articles & appends to the page
 $.getJSON("/all", function(data) {
 	for (var i = 0; i < data.length; i++) {
-		newDiv = $("<div>").addClass("article "+data[i]._id);
-		$("#allArticles").append(newDiv + "<h3>"+data[i].title+"</h3><hr><p>"+data[i].link+"</p>");
+
+		$("#allArticles").append("<div data-id='"+data[i]._id+"'>"
+			+"<h3>"+data[i].title+"</h3><hr><p>"+data[i].link+"</p><br>"
+			+"<button class='noteButton'>"+Add Note+"</button>"
+			+"<button class='deleteButton'>"+Delete Article+"</button>");
 	}
 });
 
 // Adds new note to article specified
-$(".noteButton").on("click", function() {
+// $(".noteButton").on("click", function() {
 	//Send a popup where user types a note
 	//Append the note into the popup (make sure they can add multiple notes)
-});
+// });
 
 // $(".deleteButton").on("click", function() {})
 
