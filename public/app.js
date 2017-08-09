829fe54fee57a18319b5b54e95bebd8b72e92dd8
Charlotte Astle <charlotteastle@Charlottes-MBP.local.tld>
2017-08-09 08:55:42 -0700

    json get route for all articles

--- public/app.js
+++ public/app.js
@@ -1,3 +1,12 @@
+// Gets json-version of articles & appends to the page
+$.getJSON("/all", function(data) {
+	for (var i = 0; i < data.length; i++) {
+		newDiv = $("<div>").addClass("article "+data[i]._id);
+		$("#allArticles").append(newDiv + "<h3>"+data[i].title+"</h3><hr><p>"+data[i].link+"</p>");
+	}
+});
+
+// Adds new note to article specified
 $(".noteButton").on("click", function() {
 	//Send a popup where user types a note
 	//Append the note into the popup (make sure they can add multiple notes)
